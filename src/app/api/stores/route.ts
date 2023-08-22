f83172ac4a9e4e7c66c0f5cfefac8c473b2713d2
Federico Luna <fluna@frro.utn.edu.ar>
2023-08-22 20:28:53 -0300

    first commit

---
+++ src/app/api/stores/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+export async function POST (req: Request) {
+  try {
+    const { userId } = auth()
+    const body = await req.json()
+    const { name } = body
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+
+    if (!name) {
+      return new NextResponse('Nombre requerido', { status: 400 })
+    }
+
+  } catch (error) {
+    console.log('[STORES_POST]', error);
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
